52594a7de08756e4784cf57b4e692102a3a68c61
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-03-10 22:17:39 -0400

    Create Form.jsx component. Imported it into Login.jsx for modularity and react best practices.

---
+++ src/components/Form.jsx
@@ -0,0 +1,38 @@
+
+
+
+const Form = ({ email, setEmail, password, setPassword, handleSubmit, errorMessage }) => {
+  return (
+
+    <form className="form-1" id="LoginForm" onSubmit={handleSubmit}>
+            <label>Enter Email:</label>
+            <input 
+              className="input-1"  
+              placeholder="Enter Email" 
+              type="text" 
+              id="emailField" 
+              name="emailField" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+    
+            <label >Enter Password:</label> 
+            <input 
+              className="input-2" 
+              placeholder="Enter Password" 
+              type="password" 
+              id="passwordField" 
+              name="passwordField" 
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              />
+            
+            
+                <button className="btn-1" type="submit" id="submit" name="submit" >Login</button>
+            
+                {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
+        </form>
+  )
+}
+
+export default Form;
