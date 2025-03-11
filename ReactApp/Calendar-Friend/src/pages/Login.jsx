22b3c8adab82fa80f27f9b4d78f5244703eb93b9
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-03-10 22:17:39 -0400

    Create Form.jsx component. Imported it into Login.jsx for modularity and react best practices.

--- ReactApp/Calendar-Friend/src/pages/Login.jsx
+++ ReactApp/Calendar-Friend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
-
+import Form from '../components/Form';
 
 const Login = () => {
 
@@ -30,35 +30,15 @@ const Login = () => {
 
   return (
       <div>
-        <form className="form-1" id="LoginForm" onSubmit={handleSubmit}>
-            <label>Enter Email:</label>
-            <input 
-              className="input-1"  
-              placeholder="Enter Email" 
-              type="text" 
-              id="emailField" 
-              name="emailField" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              />
-    
-            <label >Enter Password:</label> 
-            <input 
-              className="input-2" 
-              placeholder="Enter Password" 
-              type="password" 
-              id="passwordField" 
-              name="passwordField" 
-              value={password}
-              onChange={(e)=> setPassword(e.target.value)}
-              />
-            
-            
-                <button className="btn-1" type="submit" id="submit" name="submit" >Login</button>
-            
-
-        </form>
-        {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
+        <Form 
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          handleSubmit={handleSubmit}
+          errorMessage={errorMessage}
+          />
+        
       </div>
 
     )
