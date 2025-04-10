e5abb477ddde156d902e49e73dfbabad46d2e01a
Christopher Sciortino <34723997+RedstoneWiz@users.noreply.github.com>
2025-04-10 12:27:36 -0400

    Navbar Update
    Redirecting navbar, this method uses routing functions but we can use links as well. Only the My Dashboard button redirects.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 
+function NavBar({ email }) {
+  
+  const navigate = useNavigate();
+  const navigateToDashboard = () => {
+    navigate("/Dashboard", {
+      state:
+      {
+        email: email
+      }
+    });
+  };
 
-function NavBar({email}) {
   return (
   <nav class="navbar">
     <div class="logo">Calendar Friend | Welcome, <span id="email">{email}</span>! </div>
     
     <ul class="nav-links">
-        <li><a href="#" class="active">Home</a></li>
+        <li><a onClick={() => navigateToDashboard()} class="active">My Dashboard</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">My Account</a></li>
         <li><a href="Login.html">Sign Out</a></li>
