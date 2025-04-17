3c6f46c743df1bd52656b64340ed041a716179be
Christopher Sciortino <34723997+RedstoneWiz@users.noreply.github.com>
2025-04-17 12:56:13 -0400

    Fixed CSS In Class
    Minor CSS changes to UI

---
+++ src/components/MonthCalendar.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+
+export function MonthCalendar({}) {
+  const [events, setEvents] = useState([])
+
+
+  return (
+    <div>
+      <FullCalendar 
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView='dayGridMonth'
+        allDaySlot={false}
+        weekends={true}
+        selectable={true}
+        events={events}
+        aspectRatio={2}
+        eventContent={(eventInfo) => (
+          <>
+            <b>{eventInfo.timeText}</b> <i>{eventInfo.event.title}</i>
+          </>
+        )}
+      />
+    </div>
+  )
+}
+
