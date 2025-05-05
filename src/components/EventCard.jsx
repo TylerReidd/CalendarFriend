4b89d4fe026a089194ff3349b1b3bf48d14c5c14
Christopher Sciortino <34723997+RedstoneWiz@users.noreply.github.com>
2025-05-05 14:35:56 -0400

    Continue Added Features
    Added a delete button and started working on the dropdown needed in the submit available times page.

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,18 +2,49 @@ import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-const EventCard = ({title, host}) => {
+const EventCard = ({event, host, email}) => {
 
   const navigate = useNavigate();
   const navigateToEventDetails = () => {
     navigate("/eventdetails", {
       state:
       {
-        title: title,
+        event: event,
         host: host,
+        email: email
       }
     });
   };
+
+  const navigateBackToDashboard = () => {
+    navigate("/dashboard", {
+      state:
+      {
+        email: email
+      }
+    });
+  };
+
+  const declineEvent = () =>
+  {
+    fetch("http://localhost:3000/DeclineEvent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify( { event, email } )
+    })
+    .then(response => response.json())
+    .then(data =>
+    {
+      if (data.success)
+      {
+        navigateBackToDashboard();
+      }
+      else
+      {
+        console.log("Could not delete event.");
+      }
+    })
+  }
 
   return (
     <>
@@ -22,7 +53,7 @@ const EventCard = ({title, host}) => {
       <div>
         <div className="eventCardInfo">
           <h3>Event Title:</h3>
-          <p>{title}</p>
+          <p>{event}</p>
         </div>
 
         <div className="eventCardInfo">
@@ -32,6 +63,8 @@ const EventCard = ({title, host}) => {
       </div>
 
       <button onClick={() => navigateToEventDetails()} class="eventCardViewDetailsButton">View Event Details</button>
+
+      <button onClick={() => declineEvent()} class="eventCardViewDetailsButton"> Decline Event </button>
 
     </div>
     </>
