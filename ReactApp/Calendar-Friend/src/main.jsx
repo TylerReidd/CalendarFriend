decd26404141481a271530bba82e651e439e2506
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-05-07 16:52:53 -0400

    test deploy

--- ReactApp/Calendar-Friend/src/main.jsx
+++ ReactApp/Calendar-Friend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "../src/App.jsx"
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render
 (
   <StrictMode>
+    <BrowserRouter basename="/Calendar-Friend">
      <App/>
+    </BrowserRouter>
   </StrictMode>,
 )
